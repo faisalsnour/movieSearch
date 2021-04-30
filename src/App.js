0fef6a30048ce38959926a96c3742fbc4709fde6
Faisal Nour <faisalskhalifa@gmail.com>
2021-04-29 22:02:34 -0400

    search component is built

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Wrapper from './Components/Wrapper'
 import Title from './Components/Title'
+import Search from './Components/Search'
 
 function App() {
   return (
     <Wrapper>
       <Title title="The shoppies" />
+      <Search />
     </Wrapper>
   );
 }
