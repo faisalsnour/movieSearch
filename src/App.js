564e524d50d659f1ac163fc75b5bef7a76d2a5ba
Faisal Nour <faisalskhalifa@gmail.com>
2021-05-08 01:53:31 -0400

    Footer component is added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import Wrapper from './Components/Wrapper'
 // import Title from './Components/Title'
 import Results from './Components/Results'
+import Footer from './Components/Footer'
 
 
 function App() {
   return (
-    <Wrapper>
-      {/* <Title title="The shoppies" /> */}
-      <Results />
-    </Wrapper>
+    <>
+      <Wrapper>
+        {/* <Title title="The shoppies" /> */}
+        <Results />
+      </Wrapper>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./style.css"
+
+function Footer() {
+    return (
+        <>
+            <div className="footer" style={{ textAlign: "center" }}>
+                <div id="copyright" >&#169;2021. All Rights Reserved. Developed by <a aria-label="faisal" style={{ fontWeight: "bold", color: "white", textDecoration: "none" }} href="https://www.faisalnour.com" target="_blank" rel="noreferrer" aria-hidden="true">Faisal Nour</a></div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
